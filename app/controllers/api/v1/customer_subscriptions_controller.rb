72a2b3651fec014b8460ba05633e5327258d29a1
joemecha <jwmecha@gmail.com>
2021-08-04 22:44:51 -0500

    Correct code formatting

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
     customer = Customer.find(params[:id])
     subscriptions = customer.subscriptions
     if subscriptions.empty?
-      render json: {message: "There are no subscriptions for this customer."}
+      render json: { message: 'There are no subscriptions for this customer.' }
     else
       render json: SubscriptionSerializer.new(subscriptions)
     end
@@ -19,13 +19,13 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
   end
 
   def update
-    customer_sub = CustomerSubscription.find(params[:id])
+    CustomerSubscription.find(params[:id])
     if params[:customer_subscription][:customer_id].present? && params[:customer_subscription][:subscription_id].present?
       customer_sub = CustomerSubscription.find_by(customer_subscription_params)
-      
+
       cancelled_sub = CustomerSubscription.update(customer_sub.id, status: 'cancelled')
       render json: CustomerSubscriptionSerializer.new(cancelled_sub)
-    elsif 
+    else
       render json: { errors: 'Requires valid IDs for customer and subscription.' }, status: :bad_request
     end
   end
