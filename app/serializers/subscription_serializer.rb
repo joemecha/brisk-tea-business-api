a8047bc91adc04ad4eb6f9e1d2649176a1b98e0f
joemecha <jwmecha@gmail.com>
2021-08-06 08:20:07 -0500

    Remove relation teas from customersubscription serializer

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,5 +1,4 @@
 class SubscriptionSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :price, :status, :frequency
-  has_many :teas, if: proc { |record| record.teas.any? }
 end
