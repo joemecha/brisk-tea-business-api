913803f14dae8e3cc213b29fb96860658384041e
joemecha <jwmecha@gmail.com>
2021-09-13 17:22:46 -0500

    Update customer model relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,6 @@
 class Customer < ApplicationRecord
-  has_many :customer_subscriptions, dependent: :destroy
-  has_many :subscription_teas, through: :subscriptions
-  has_many :subscriptions, through: :customer_subscriptions
-  has_many :teas, through: :subscription_teas
+  has_many :subscriptions, dependent: :destroy
+  has_many :teas, through: :subscriptions
   
   validates :first_name, :last_name, :email, :street, :city, :state, :zip, presence: true
 end
